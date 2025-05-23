4a466b6ac9d49730c9ce75315e08bef2a68059c4
AnthonyGardelle <a.gardelle18@gmail.com>
2025-05-23 14:22:28 +0200

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/scss/app.scss', 'resources/js/app.js'],
