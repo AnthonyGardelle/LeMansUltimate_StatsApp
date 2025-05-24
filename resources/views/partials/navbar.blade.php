6784b2114121c543fcdeba866ba20f4ee7f17437
AnthonyGardelle <a.gardelle18@gmail.com>
2025-05-24 11:10:35 +0200

    Enhance navbar profile link by wrapping user name in a paragraph and adding overflow handling

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,11 +1,13 @@
 <nav>
     <ul>
         <li><a href="{{ url('/') }}" class="{{ request()->is('/') ? 'active' : '' }}">@lang('message.accueil')</a></li>
-        <li><a href="{{ url('results') }}" class="{{ request()->is('results') ? 'active' : '' }}">@lang('message.result')</a></li>
+        <li><a href="{{ url('results') }}"
+                class="{{ request()->is('results') ? 'active' : '' }}">@lang('message.result')</a></li>
         @guest
             <li><a href="{{ route('show.register') }}"
                     class="{{ request()->is('register') ? 'active' : '' }}">@lang('message.register')</a></li>
-            <li><a href="{{ route('show.login') }}" class="{{ request()->is('login') ? 'active' : '' }}">@lang('message.login')</a>
+            <li><a href="{{ route('show.login') }}"
+                    class="{{ request()->is('login') ? 'active' : '' }}">@lang('message.login')</a>
             </li>
         @else
             <li>
@@ -15,8 +17,9 @@
                 <a href="#" onclick="return showLogoutPopup(event);">@lang('message.logout')</a>
             </li>
             <li><a id="profil-link" href="{{ route('show.profile') }}"
-                    class="{{ request()->is('profile') ? 'active' : '' }}">{{ Auth::user()->first_name }}
-                    {{ Auth::user()->name }}<img src="{{ asset('storage/' . Auth::user()->image)}}" alt="">
+                    class="{{ request()->is('profile') ? 'active' : '' }}">
+                    <p>{{ Auth::user()->first_name }} {{ Auth::user()->name }}</p>
+                    <img src="{{ asset('storage/' . Auth::user()->image)}}" alt="">
                 </a>
             </li>
         @endguest
