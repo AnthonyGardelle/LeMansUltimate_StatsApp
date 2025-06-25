1a156e2b05bcaa1dcf6689fb2db3fd0493edbd55
AnthonyGardelle <a.gardelle18@gmail.com>
2025-06-25 23:58:53 +0200

    Define table name for LmuCompound model

--- app/Models/LmuCompound.php
+++ app/Models/LmuCompound.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class LmuCompound extends Model
 {
+
+    protected $table = 'lmu_compounds';
+
     protected $fillable = [
         'front_compound',
         'rear_compound'
